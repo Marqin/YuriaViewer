2256177cb94e2a9739cca33fad1094b357b06dae
Marqin <Marqin@users.noreply.github.com>
2014-03-02 20:24:03 +0100

    fix OS X build

--- compile_osx.sh
+++ compile_osx.sh
@@ -1,4 +1,4 @@
 # TODO: cmake
 
 mkdir -p bin
-clang -lm -lGLEW -lglfw3 -framework IOKit -framework OpenGL -framework Cocoa -Wall -Werror -Wextra -pedantic -O2 -std=c99 fractal.c callbacks.c main.c -o bin/YuriaViewer
+clang -lm -lGLEW -lglfw3 -framework QuartzCore -framework IOKit -framework OpenGL -framework Cocoa -Wall -Werror -Wextra -pedantic -O2 -std=c99 fractal.c callbacks.c main.c -o bin/YuriaViewer
